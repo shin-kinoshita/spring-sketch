249b52beae41614f6064e8d4d1ca3b92a8b9ca52
shin-kinoshita <kinoshita116@gmail.com>
2018-09-08 16:31:31 +0900

    rename file and path

--- src/main/java/com/example/sketch/request/binding/simple/text/SimpleTextController.java
+++ src/main/java/com/example/sketch/request/binding/simple/text/SimpleTextController.java
@@ -1,4 +1,4 @@
-package com.example.sketch.binding.simple.input;
+package com.example.sketch.request.binding.simple.text;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author subaru
  */
 @Controller
-@RequestMapping("/binding/simple")
-public class SimpleController {
+@RequestMapping("/binding/simple/text")
+public class SimpleTextController {
     @GetMapping("input")
     public String input(Model model) {
-        model.addAttribute("simpleForm", new SimpleForm());
-        return "binding/simple/input/input";
+        model.addAttribute("simpleTextForm", new SimpleTextForm());
+        return "binding/simple/text/input";
     }
 
     @PostMapping("output")
-    public String output(@ModelAttribute SimpleForm simpleForm) {
-        return "binding/simple/input/output";
+    public String output(@ModelAttribute SimpleTextForm simpleTextForm) {
+        return "binding/simple/text/output";
     }
 }
